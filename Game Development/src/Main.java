0a161a4c958b5edb4c8f73c525831d98dbde2d15
LuigiConti <luigi09c@gmail.com>
2018-05-02 20:55:39 -0400

    zipee

--- Game Development/src/Main.java
+++ Game Development/src/Main.java
@@ -4,5 +4,7 @@ public class Main {
 		System.out.println("Ready to start coding, Luigi?"); // Yes!
 		System.out.println("It shouldn't be this bad...");
 		System.out.println("Yes. It shouldn't...");
+		System.out.println("Programming robot... beep boop!");
 	}
+	
 }
