6998bd9982c10c5d2e8edd7a7594071933893951
Poonchy <Poonchy@LAPTOP-KS6R9PBP.home>
2018-05-03 16:34:15 -0400

    Added new button

--- Game Development/src/Main.java
+++ Game Development/src/Main.java
@@ -1,6 +1,8 @@
+import java.awt.event.ActionListener;
+
 import javax.swing.JFrame;
 
-public class Main extends JFrame{
+public abstract class Main extends JFrame implements ActionListener{
 	public static void main(String[] args) {
 		
 		//Creating Main Menu for game.
--- Game Development/src/MainMenu.java
+++ Game Development/src/MainMenu.java
@@ -1,19 +1,44 @@
 import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 
-public class MainMenu {
+public class MainMenu{
+	
 	public void createMenu() {
 		JFrame Menu = new JFrame("Main Menu");
-		Menu.getContentPane().add(startButton());
 		Menu.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		Menu.getContentPane().setBackground(Color.red); 
 		Menu.setSize(1200, 700);
+		Menu.getContentPane().add(startButton());
+		Menu.getContentPane().add(helpButton());
+		Menu.getContentPane();
 		Menu.setVisible(true);
 	}
+	
 	public JButton startButton() {
 		JButton startBtn = new JButton("Start Game");
-		startBtn.setSize(100,20);
+		startBtn.setSize(100,40);
+		startBtn.addActionListener(new ActionListener(){
+		    @Override
+		    public void actionPerformed(ActionEvent e) {
+		        System.out.println("This would start the game!");
+		    }
+		});
 		return startBtn;
 	}
+	
+	public JButton helpButton() {
+		JButton helpBtn = new JButton("Help");
+		helpBtn.setSize(100,40);
+		helpBtn.addActionListener(new ActionListener(){
+		    @Override
+		    public void actionPerformed(ActionEvent e) {
+		        System.out.println("This would show instructions!");
+		    }
+		});
+		return helpBtn;
+	}
+	
 }
