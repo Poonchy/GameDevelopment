7ae971ba9677911a2515b6a8df5a12c29e0b5d83
LuigiConti <luigi09c@gmail.com>
2018-05-02 21:03:49 -0400

    wooopteedoo

--- Game Development/src/Main.java
+++ Game Development/src/Main.java
@@ -2,10 +2,11 @@ public class Main {
 	public static void main (String[] args) {
 		System.out.println("Let's start working!");
 		System.out.println("Ready to start coding, Luigi?"); // Yes!
-		System.out.println("It shouldn't be this bad...");
-		/*System.out.println("Yes. It shouldn't...");
-		System.out.println("Programming robot... beep boop!");
+		System.out.println("It shouldn't be thIS                      As bad...");
+		/* System.out.println("Yes. It shouldn't...");
+		System.out.println("Programming robot... beep   boop!");
 		System.out.println("This is called merging!"); */
+		//Testing thing
 		
 	}
 }
