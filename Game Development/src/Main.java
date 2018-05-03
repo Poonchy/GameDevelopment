3c809bef61afe444db9b52e120fa20f04d682c2b
Poonchy <Poonchy@gmail.com>
2018-05-02 20:36:04 -0400

    Trying to get this to work!

--- Game Development/src/Main.java
+++ Game Development/src/Main.java
@@ -2,5 +2,6 @@ public class Main {
 	public static void main (String[] args) {
 		System.out.println("Let's start working!");
 		System.out.println("Ready to start coding, Luigi?");
+		System.out.println("It shouldn't be this bad...");
 	}
 }
