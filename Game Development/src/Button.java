9afe60b3ceefa55c545b965cff48c70c4f274cb3
Poonchy <Poonchy@10.255.31.149>
2018-05-03 18:36:57 -0400

    Added button constructor and button construction in MenuState.

--- Game Development/src/Button.java
+++ Game Development/src/Button.java
@@ -1,7 +1,13 @@
 
 public class Button {
-	
-	public Button() {
-		
+	public int width;
+	public int height;
+	public int xPoS;
+	public int yPoS;
+	public Button(int widtht, int heightt, int xPoSt, int yPoSt) {
+		height = heightt;
+		width = widtht;
+		xPoS = xPoSt;
+		yPoS = yPoSt;
 	}
 }
--- Game Development/src/MenuState.java
+++ Game Development/src/MenuState.java
@@ -13,7 +13,9 @@ public class MenuState{
 	}
 	
 	public void update() {
-		
+		Button startbtn, endbtn;
+		startbtn = new Button(100, 40, 20, 20);
+		endbtn = new Button(100, 40, 20, 80);
 	}
 	
 	public void render(Graphics g) {
