06e029f0dc5f51e4e4e1b78dad2b5fd181002d6a
LuigiConti <luigi09c@gmail.com>
2018-05-02 21:00:46 -0400

    commit comet

--- Game Development/src/Main.java
+++ Game Development/src/Main.java
@@ -3,9 +3,10 @@ public class Main {
 		System.out.println("Let's start working!");
 		System.out.println("Ready to start coding, Luigi?"); // Yes!
 		System.out.println("It shouldn't be this bad...");
-		System.out.println("Yes. It shouldn't...");
+		/*System.out.println("Yes. It shouldn't...");
 		System.out.println("Programming robot... beep boop!");
-		System.out.println("This is called merging!");
+		System.out.println("This is called merging!"); */
+		
 	}
 	
 }
