545aa0a83a7f9ace0ead64b1d069ff22ac1f152f
LuigiConti <luigi09c@gmail.com>
2018-05-02 20:45:16 -0400

    yoyoyo

--- Game Development/src/Main.java
+++ Game Development/src/Main.java
@@ -1,7 +1,8 @@
 public class Main {
 	public static void main (String[] args) {
 		System.out.println("Let's start working!");
-		System.out.println("Ready to start coding, Luigi?");
+		System.out.println("Ready to start coding, Luigi?"); // Yes!
 		System.out.println("It shouldn't be this bad...");
+		System.out.println("Yes. It shouldn't....");
 	}
 }
